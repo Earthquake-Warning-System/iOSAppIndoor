8536ade04ea36cf384d7a98bed156903e7ea8a5f
zhe114 <james87924@gmail.com>
2020-06-11 14:15:38 +0800

    Modify some instuction.

--- iOSAppIndoor/AppDelegate.swift
+++ iOSAppIndoor/AppDelegate.swift
@@ -50,7 +50,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         print("application Will Enter Background")
         
-        // now, do what you want to do
         
     }
     
